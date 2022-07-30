ee753caebcf2363a56b3bffd9c671703c6e7c46c
Caio <caiosobreira77@gmail.com>
2022-07-29 21:13:54 -0300

    Final version fix wrong words

--- DrivenDB.sql
+++ DrivenDB.sql
@@ -25,7 +25,7 @@ CREATE TABLE "transactions" (
 	"bankAccountId" INTEGER NOT NULL REFERENCES "bankAccount"("id"),
 	"amount" NUMBER NOT NULL ,
 	"type" TEXT NOT NULL,
-	"time" TIMESTAMP NOT NULL DEFAUFALSE
+	"time" TIMESTAMP NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE "creditCards" (
